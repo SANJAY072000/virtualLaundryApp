666d07a35725e8f18fe71a36b841100f9353942c
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-06-04 16:12:01 +0530

    Added client react-native

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Welcome to the app</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
