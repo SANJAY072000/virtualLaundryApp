ecf427eac7c27487b06e9e9e735a4919931a7bb4
SANJAY SINGH BISHT <39243846+SANJAY072000@users.noreply.github.com>
2021-04-30 22:25:31 +0530

    Done with app video

--- setup/config.js
+++ setup/config.js
@@ -1,6 +1,6 @@
 // exporting the configurations
 module.exports={
-    mongoUrl:'mongodb+srv://sanjay_20:san07jay@cluster0-yxg19.mongodb.net/test?retryWrites=true&w=majority',
+    mongoUrl:'',
     secret:'Work Hard'
 };
 
